4c665ceb6eeb4faf7d96008ea71aaa5e53102670
moyasine <mr.hatoto@gmail.com>
2018-03-25 10:50:47 +0900

    style change

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -2,13 +2,17 @@ class Presentation < ActiveRecord::Base
 
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
-      # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
-      presentation = find_by(id: row["id"]) || new
-      # CSVからデータを取得し、設定する
-      presentation.attributes = row.to_hash.slice(*updatable_attributes)
-      # 保存する
-      presentation.attributes = {upload_on: Date.today.strftime('%Y-%m-%d')}
-      presentation.save!
+      unless row["body"] == ""
+        # IDが見つかれば、レコードを呼び出し、見つかれなければ、新しく作成
+        presentation = find_by(id: row["id"]) || new
+        # CSVからデータを取得し、設定する
+        presentation.attributes = row.to_hash.slice(*updatable_attributes)
+        # 保存する
+        presentation.attributes = {upload_on: Date.today.strftime('%Y-%m-%d')}
+        presentation.save!
+      end
+
+
     end
   end
 
